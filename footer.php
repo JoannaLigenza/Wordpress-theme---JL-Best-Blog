5a9cea1c5ce687b7d3aeed46ffac1cadd36fce28
JoannaLigenza <ligenzajoanna@gmail.com>
2019-12-12 20:29:32 +0100

    add footer

---
+++ footer.php
@@ -0,0 +1,8 @@
+        <footer class="footer container">
+            <p><?php bloginfo('name'); ?> - &copy <?php echo date('Y'); ?></p>
+        </footer>
+
+<?php wp_footer(); ?> 
+    </div>      <!-- main-container -->
+</body>
+</html>
