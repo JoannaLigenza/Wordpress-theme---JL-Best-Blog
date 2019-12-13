103b0dd287d994fe75e03a448a55b7353d54ed83
JoannaLigenza <ligenzajoanna@gmail.com>
2019-12-13 11:15:11 +0100

    logo and header image added

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
-    // Add styles
+    // Include styles
     function enqueue_style_myTheme() {
         wp_enqueue_style( 'style', get_stylesheet_uri() );
     }
     add_action( 'wp_enqueue_scripts', 'enqueue_style_myTheme' );
 
-    // Register menus
+    // Include menus
     function register_my_menus() {
         register_nav_menus(
             array(
@@ -15,6 +15,45 @@
         );
     }
     add_action( 'init', 'register_my_menus' );
+
+    // Include logo
+    function set_custom_logo() {
+        $defaults = array(
+        'height'      => 100,
+        'width'       => 120,
+        'flex-height' => true,
+        'flex-width'  => true,
+        // 'header-text' => array( 'site-title', 'site-description' ),
+        );
+        add_theme_support( 'custom-logo', $defaults );
+    }
+    add_action( 'after_setup_theme', 'set_custom_logo' );
+
+    // Include custom header
+    function set_custom_header() {
+        $header_info = array(
+            'flex-width'    => true,
+            'width'         => 1200,
+            'flex-height'   => true,
+            'height'        => 200,
+            'default-image' => get_template_directory_uri() . '/images/logo3.png',
+            // Display the header text along with the image
+            'header-text'   => true,
+        );
+        add_theme_support( 'custom-header', $header_info );
+
+        // register header(s)
+        $header_images = array(
+            'sunset' => array(
+                    'url'           => get_template_directory_uri() . '/images/logo3.png',
+                    'thumbnail_url' => get_template_directory_uri() . '/images/logo3.png',
+                    'description'   => 'Sunset',
+            ),
+        );
+        register_default_headers( $header_images );
+    }
+    add_action( 'after_setup_theme', 'set_custom_header' );
+    
 
 
 
--- header.php
+++ header.php
@@ -11,9 +11,39 @@
 </head>
 <body <?php body_class(); ?> >
     <div class="main-container">
-        <header class="header container">
-            <h2><a href="<?php echo home_url() ?>"><?php bloginfo('name') ?></a></h2>
-            <h5><?php bloginfo('description') ?></h5>
+        <header class="header">
+            <div class="header-title container">
+                <!-- Displaying logo -->
+                <?php if ( has_custom_logo() ) { ?>
+                    <div class="logo">
+                        <?php
+                            if ( function_exists( 'the_custom_logo' ) ) {
+                                the_custom_logo();
+                            }
+                        ?>
+                    </div>
+                <?php } ?>
+                <!-- Displaying site title and description -->
+                <?php
+                    if ( display_header_text() ){ ?>
+                        <div class="title">
+                            <h2><a href="<?php echo home_url() ?>"><?php bloginfo('name') ?></a></h2>
+                            <h5><?php bloginfo('description') ?></h5>
+                        </div>                           
+                    <?php } else{
+                        //do something
+                    }
+                ?>
+            </div>
+            <div class="header-image container">
+                <?php if ( get_header_image() ) : ?>
+                    <div id="site-header">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                            <img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+                        </a>
+                    </div>
+                <?php endif; ?>
+            </div>
             <nav>
                 <?php 
                     wp_nav_menu(
