e85d7b7db97aac592a1b6711af07f29ade473def
JoannaLigenza <ligenzajoanna@gmail.com>
2019-12-20 21:05:32 +0100

    added link to page of author theme

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
         <footer class="footer">
             <div class="container">
-                <p><?php bloginfo('name'); ?> - &copy <?php echo date('Y'); ?></p>
+                <!-- <p><?php bloginfo('name'); ?> - &copy <?php echo date('Y'); ?></p> -->
+                <p class="theme-author">Theme made by: <a href="https://love-coding.pl/en"><span class="theme-author-link">JL</span></a></p>
             </div>
             
         </footer>
