ddde908fe08c0c8f4499b007000fb9944bb12b7e
JoannaLigenza <ligenzajoanna@gmail.com>
2019-12-31 17:47:00 +0100

    deleted author and date files

--- author.php
+++
@@ -1,6 +0,0 @@
-<?php
-    get_header();
-    echo "<h3 class='specific-pages-title container'>Author: ".get_the_author()." </h3>";
-    get_template_part( 'content-excerpt' );
-    get_footer();
-?>
--- date.php
+++
@@ -1,6 +0,0 @@
-<?php
-    get_header();
-    echo "<h3 class='specific-pages-title container'>Date: ".get_the_date( 'Y-m' )." </h3>";
-    get_template_part( 'content-excerpt' );
-    get_footer();
-?>
