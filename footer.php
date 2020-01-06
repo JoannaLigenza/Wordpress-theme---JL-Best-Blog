186ad03820a74acffea678d039007867f6930fd5
JoannaLigenza <ligenzajoanna@gmail.com>
2020-01-06 14:28:17 +0100

    added escaping functions for footer.php

--- footer.php
+++ footer.php
@@ -34,64 +34,64 @@
                     <!-- facebook icon -->
                     <?php
                     if (get_theme_mod( 'social-icon-facebook' ) !== "") { ?>
-                        <a href="<?php echo get_theme_mod( 'social-icon-facebook' ) ?>">
-                            <img src="<?php echo get_theme_file_uri() . '/images/facebook-icon.png' ?>" alt="facebook-icon" class="social-icon">
+                        <a href="<?php echo esc_attr( get_theme_mod( 'social-icon-facebook' ) ); ?>">
+                            <img src="<?php echo esc_attr( get_theme_file_uri() . '/images/facebook-icon.png' ); ?>" alt="facebook-icon" class="social-icon">
                         </a>
                     <?php
                     } ?>
                     <!-- instagram icon -->
                     <?php
                     if (get_theme_mod( 'social-icon-instagram' ) !== "") { ?>
-                        <a href="<?php echo get_theme_mod( 'social-icon-instagram' ) ?>">
-                            <img src="<?php echo get_theme_file_uri() . '/images/instagram-icon.png' ?>" alt="instagram-icon" class="social-icon">
+                        <a href="<?php echo esc_attr( get_theme_mod( 'social-icon-instagram' ) ); ?>">
+                            <img src="<?php echo esc_attr( get_theme_file_uri() . '/images/instagram-icon.png' ); ?>" alt="instagram-icon" class="social-icon">
                         </a>
                     <?php
                     } ?>
                     <!-- twitter icon -->
                     <?php
                     if (get_theme_mod( 'social-icon-twitter' ) !== "") { ?>
-                        <a href="<?php echo get_theme_mod( 'social-icon-twitter' ) ?>">
-                            <img src="<?php echo get_theme_file_uri() . '/images/twitter-icon.png' ?>" alt="twitter-icon" class="social-icon">
+                        <a href="<?php echo esc_attr( get_theme_mod( 'social-icon-twitter' ) ); ?>">
+                            <img src="<?php echo esc_attr( get_theme_file_uri() . '/images/twitter-icon.png' ); ?>" alt="twitter-icon" class="social-icon">
                         </a>
                     <?php
                     } ?>
                     <!-- pinterest icon -->
                     <?php
                     if (get_theme_mod( 'social-icon-pinterest' ) !== "") { ?>
-                        <a href="<?php echo get_theme_mod( 'social-icon-pinterest' ) ?>">
-                            <img src="<?php echo get_theme_file_uri() . '/images/pinterest-icon.png' ?>" alt="pinterest-icon" class="social-icon">
+                        <a href="<?php echo esc_attr( get_theme_mod( 'social-icon-pinterest' ) ); ?>">
+                            <img src="<?php echo esc_attr( get_theme_file_uri() . '/images/pinterest-icon.png' ); ?>" alt="pinterest-icon" class="social-icon">
                         </a>
                     <?php
                     } ?>
                     <!-- youtube icon -->
                     <?php
                     if (get_theme_mod( 'social-icon-youtube' ) !== "") { ?>
-                        <a href="<?php echo get_theme_mod( 'social-icon-youtube' ) ?>">
-                            <img src="<?php echo get_theme_file_uri() . '/images/youtube-icon.png' ?>" alt="youtube-icon" class="social-icon">
+                        <a href="<?php echo esc_attr( get_theme_mod( 'social-icon-youtube' ) ); ?>">
+                            <img src="<?php echo esc_attr( get_theme_file_uri() . '/images/youtube-icon.png' ); ?>" alt="youtube-icon" class="social-icon">
                         </a>
                     <?php
                     } ?>
                     <!-- whatsapp icon -->
                     <?php
                     if (get_theme_mod( 'social-icon-whatsapp' ) !== "") { ?>
-                        <a href="<?php echo get_theme_mod( 'social-icon-whatsapp' ) ?>">
-                            <img src="<?php echo get_theme_file_uri() . '/images/whatsapp-icon.png' ?>" alt="whatsapp-icon" class="social-icon">
+                        <a href="<?php echo esc_attr( get_theme_mod( 'social-icon-whatsapp' ) ); ?>">
+                            <img src="<?php echo esc_attr( get_theme_file_uri() . '/images/whatsapp-icon.png' ); ?>" alt="whatsapp-icon" class="social-icon">
                         </a>
                     <?php
                     } ?>
                     <!-- messenger icon -->
                     <?php
                     if (get_theme_mod( 'social-icon-messenger' ) !== "") { ?>
-                        <a href="<?php echo get_theme_mod( 'social-icon-messenger' ) ?>">
-                            <img src="<?php echo get_theme_file_uri() . '/images/messenger-icon.png' ?>" alt="messenger-icon" class="social-icon">
+                        <a href="<?php echo esc_attr( get_theme_mod( 'social-icon-messenger' ) ); ?>">
+                            <img src="<?php echo esc_attr( get_theme_file_uri() . '/images/messenger-icon.png' ); ?>" alt="messenger-icon" class="social-icon">
                         </a>
                     <?php
                     } ?>
                     <!-- linkedin icon -->
                     <?php
                     if (get_theme_mod( 'social-icon-linkedin' ) !== "") { ?>
-                        <a href="<?php echo get_theme_mod( 'social-icon-linkedin' ) ?>">
-                            <img src="<?php echo get_theme_file_uri() . '/images/linkedin-icon.png' ?>" alt="linkedin-icon" class="social-icon">
+                        <a href="<?php echo esc_attr( get_theme_mod( 'social-icon-linkedin' ) ); ?>">
+                            <img src="<?php echo esc_attr( get_theme_file_uri() . '/images/linkedin-icon.png' ); ?>" alt="linkedin-icon" class="social-icon">
                         </a>
                     <?php
                     } ?>
@@ -99,8 +99,8 @@
                     <?php
                 }
                 ?>  <!-- end of social icons section -->
-                <p class="footer-link"><small><?php _e( 'Theme made by: ', 'myfirsttheme') ?></small><a href="https://love-coding.pl/en" class="footer-theme-link">JL</a><small>, 
-                    <?php _e( 'used by: ', 'myfirsttheme') ?> <a href="<?php echo home_url(); ?>" class="footer-theme-link"><?php echo bloginfo('name') ?></a></small></p>
+                <p class="footer-link"><small><?php _e( 'Theme made by: ', 'myfirsttheme') ?></small><a href="<?php echo esc_url( 'https://love-coding.pl/en' ); ?>" class="footer-theme-link">JL</a><small>, 
+                    <?php _e( 'used by: ', 'myfirsttheme') ?> <a href="<?php echo esc_url( home_url() ); ?>" class="footer-theme-link"><?php echo bloginfo('name') ?></a></small></p>
             </div>
             
         </footer>
