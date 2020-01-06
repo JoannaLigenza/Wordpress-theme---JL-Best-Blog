c03b07d47a0fdd7b52aa27b1e32420214beeac90
JoannaLigenza <ligenzajoanna@gmail.com>
2020-01-06 19:04:56 +0100

    added escape functions for searchform file

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<input type="search"  class="search-input" placeholder="Szukaj..." value="<?php echo get_search_query(); ?>" name="s" />
-	<button type="submit" class="search-submit"><img id="ff" src="<?php echo get_theme_file_uri().'/images/search-icon.png' ?>"></button>
+	<button type="submit" class="search-submit"><img id="ff" src="<?php echo esc_url( get_theme_file_uri().'/images/search-icon.png' ) ?>"></button>
 </form>
