9b49fb12ba420860d5d59760fcf965a73391d84f
JoannaLigenza <ligenzajoanna@gmail.com>
2020-01-10 21:59:22 +0100

    add headings again

--- template-parts/content-excerpt.php
+++ template-parts/content-excerpt.php
@@ -53,7 +53,7 @@
                         } else {
                             echo "<article class='article'>";
                         }
-                        echo "<h2><a href='".esc_url( get_permalink() )."'>".esc_html( get_the_title() )."</a></h2>";
+                        echo "<heading><h2><a href='".esc_url( get_permalink() )."'>".esc_html( get_the_title() )."</a></h2><heading>";
                         if (get_theme_mod( 'post-meta' )) {
                             // $id = get_the_author_meta('ID');
                             $id = get_the_author_meta('ID');
