5b39f32e31645c4c8aec017b3f5e4824f80fbc44
JoannaLigenza <ligenzajoanna@gmail.com>
2020-01-13 18:56:33 +0100

    escape function added

--- search.php
+++ search.php
@@ -31,7 +31,7 @@
                     <article id="post-<?php the_ID(); ?>" <?php post_class( 'article image-'.esc_attr( $imagePosition ) ); ?>>
                     <?php
                         if ( has_post_thumbnail() ) {
-                            echo "<div class='image-container image-container-".esc_attr( $imagePosition )."'><a href='".get_permalink()."'>" ;
+                            echo "<div class='image-container image-container-".esc_attr( $imagePosition )."'><a href='".esc_url( get_permalink() )."'>" ;
                                 if ($imagePosition === 'above') {
                                     $imageWidth = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "full" );
                                     $maxWidth = $imageWidth[1];
@@ -46,7 +46,9 @@
                                     echo "</a></div>" ;
                                     echo "<section class='section'>";
                                 } else {
-                                    echo the_post_thumbnail( 'medium' );
+                                    echo the_post_thumbnail( 'medium', array(
+                                        'alt' => 'post-image'
+                                    ) );
                                     echo "</a></div>" ;
                                     echo "<section class='section section-padding'>";
                                 }
