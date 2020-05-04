614537ab9743058f45e21b8d7692a772b3614cc9
JoannaLigenza <ligenzajoanna@gmail.com>
2020-05-04 20:45:28 +0200

    corrected errors on comments page

--- comments.php
+++ comments.php
@@ -1,4 +1,3 @@
-
 <?php
     /**
      * This is the template for displaying Comments.
@@ -10,7 +9,7 @@
                 <h2 class="comments-title">
                     <?php 
                         $commentsNumber = get_comments_number();
-                        echo _e('COMMENTS: ', 'jl-best-blog').absint( $commentsNumber );
+                        echo esc_html_e('COMMENTS: ', 'jlbestblog').absint( $commentsNumber );
                     ?>  
                 </h2>
             </header>
@@ -26,13 +25,13 @@
             <!-- Comments navigation -->
             <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
                 <nav class="navigation comment-navigation" role="navigation">
-                    <div class="comment-nav nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'jl-best-blog' ) ); ?></div>
-                    <div class="comment-nav nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'jl-best-blog' ) ); ?></div>
+                    <div class="comment-nav nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'jlbestblog' ) ); ?></div>
+                    <div class="comment-nav nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'jlbestblog' ) ); ?></div>
                 </nav>
             <?php endif; ?>
             <!-- if comments are closed -->
             <?php if ( ! comments_open() && get_comments_number() ) : ?>
-                <p class="no-comments"><?php _e( 'Comments are closed.' , 'jl-best-blog' ); ?></p>
+                <p class="no-comments"><?php esc_html_e( 'Comments are closed.' , 'jlbestblog' ); ?></p>
             <?php endif; ?>
         </section>
     <?php
