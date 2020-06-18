2bac448da9c5cb6e7d56898f5752f06999979369
JoannaLigenza <ligenzajoanna@gmail.com>
2020-06-18 14:44:04 +0200

    changed focus order on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,48 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
     jlbestblog_toggleMobileTopMenu();
+
+    // Set tabindex order on mobile
+    function jlbestblog_set_mobile_elements_order() {
+        const skip_link = document.querySelector(".skip-link");
+        const last_menu_element = document.querySelector(".main-menu-class ul").lastElementChild.querySelector("[href]:last-child");
+        const search_submit = document.querySelector(".search-submit");
+        const last_top_menu_element = document.querySelector(".top-menu-class ul").lastElementChild.querySelector("[href]:last-child");
+        const main_content = document.getElementById("main-content--section");
+        const focusable_content_element = main_content.querySelector('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+        const last_focusable_content_element = main_content.lastElementChild.querySelector('button:last-child, [href]:last-child, input:last-child, select:last-child, textarea:last-child, [tabindex]:not([tabindex="-1"]):last-child');
+        const column = document.querySelector(".column");
+        const last_focusable_column_element = column.lastElementChild.querySelector('button:last-child, [href]:last-child, input:last-child, select:last-child, textarea:last-child, [tabindex]:not([tabindex="-1"]):last-child');
+        const footer = document.querySelector(".footer");
+console.log(last_top_menu_element)
+        skip_link.addEventListener("blur", function() {
+            document.getElementById("mobile-menu-icon").focus();
+        });
+
+        last_menu_element.addEventListener("blur", function() {
+            document.getElementById("navigation").classList.toggle("isVisible");
+            document.querySelector(".header-text").focus();
+        });
+
+        search_submit.addEventListener("blur", function() {
+            document.getElementById("mobile-top-menu-container").focus();
+        });
+
+        last_top_menu_element.addEventListener("blur", function() {
+            document.querySelector(".top-menu-class").classList.toggle("isVisible");
+            focusable_content_element.focus();
+        });
+
+        last_focusable_content_element.addEventListener("blur", function() {
+            column.querySelector('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])').focus();
+        });
+
+        last_focusable_column_element.addEventListener("blur", function() {
+            footer.querySelector('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])').focus();
+            console.log(footer)
+        });
+    }
+    jlbestblog_set_mobile_elements_order();
     
 });
 
